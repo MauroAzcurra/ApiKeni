4e29ad2e55bceb14c6db2a4f21472334d1ffe627
MauroAzcurra <137190861+MauroAzcurra@users.noreply.github.com>
2024-10-18 15:59:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const downloaderRouter = require('./routes/downloader.js');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Disini Untuk Route Nya
+
+app.use('/api/youtube', downloaderRouter);
+app.use('/api/instagram', downloaderRouter);
+app.use('/api/tiktok', downloaderRouter);
+app.use('/api/facebook', downloaderRouter);
+app.use('/api/twitter', downloaderRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port:${PORT}`);
+});
